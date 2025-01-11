a28fbed0a91dbbdfcddc2f0ea5f029de939dea10
Dimex19 <azeezabolaji06gmail.com>
2025-01-11 14:55:04 +0100

    authentication done

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require("./src/config/db");
 const userRouter = require('./src/router/userRoute'); // Adjust the path to your router file
+const path = require('path');
 
 const app = express()
 const PORT = 3878
@@ -9,7 +10,10 @@ app.use(express.json());
 app.use('/api', userRouter); // '/api/signup' will map to the signup route
 
 app.get("/", (req, res)=> {
-    res.send("<h1>Hello World</h1>")
+    const options = {
+        root: path.join(__dirname)
+    }
+    res.sendFile("index.html", options)
 })
 
 app.listen(PORT, console.log("running on colos", PORT))
