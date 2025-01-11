b5b1efd4d37e0d34cc2a1efc724989cdba14a315
Dimex19 <azeezabolaji06gmail.com>
2025-01-11 23:48:43 +0100

    cors enabled

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const cors = require('cors');
+
 require("./src/config/db");
 const userRouter = require('./src/router/userRoute'); // Adjust the path to your router file
 const path = require('path');
@@ -6,6 +8,7 @@ const path = require('path');
 const app = express()
 const PORT = 3878
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', userRouter); // '/api/signup' will map to the signup route
 
