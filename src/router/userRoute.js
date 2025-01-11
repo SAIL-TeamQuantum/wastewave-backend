06302068239a9b210ab82b8a12cbdb011c60e357
Dimex19 <azeezabolaji06gmail.com>
2025-01-11 22:55:50 +0100

    signup endpoint changed

--- src/router/userRoute.js
+++ src/router/userRoute.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { signUpUser, signInUser, updateUser, getAllUser, getUserById} = require('../controller/userController')
 
-router.route('/signup').post(signUpUser)
+router.route('/register').post(signUpUser)
 router.route('/signin').post(signInUser)
 router.route('/users').get(getAllUser)
 router.route('/user/:id').get(getUserById)
